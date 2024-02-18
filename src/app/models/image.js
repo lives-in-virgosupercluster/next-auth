74623b512fe50c4409e1f35ae6a302abdb24a892
Harsh <harsh.singh0310@gmail.com>
2024-02-18 22:46:35 +0530

    removed comments

--- src/app/models/image.js
+++ src/app/models/image.js
@@ -1,7 +1,6 @@
 import mongoose, { Schema } from "mongoose";
-// console.log("here again",process.env.MONGODB_URI);
-// mongoose.connect(process.env.MONGODB_URI);
-// mongoose.Promise = global.Promise;
+
+
 
 const imageSchema = new Schema(
   {
